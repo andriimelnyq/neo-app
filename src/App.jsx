e58853c4564492a59422594884f5ef8588667692
Andrii Melnyk <andriimelnyq@gmail.com>
2023-07-07 12:25:55 +0300

    Added error and loading handling, media queries

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
 import { useState, useEffect } from 'react';
+import { Snackbar, Alert } from '@mui/material';
 import { getNeosByDate } from './api';
-import { NeosList } from './components/NeosList';
+import { NeosList } from './components/NeoList';
+import { Loader } from './components/Loader';
 
 function App() {
   const [initialData, setInitialData] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   const loadNeos = async () => {
-    const dataFromServer = await getNeosByDate();
+    try {
+      setIsLoading(true);
+      const dataFromServer = await getNeosByDate();
 
-    const sortedInitialNeos = Object.entries(dataFromServer.near_earth_objects).sort(([dateA], [dateB]) => {
-      return new Date(dateA) - new Date(dateB);
-    });
-  
-    setInitialData(sortedInitialNeos);
+      const sortedInitialNeos = Object.entries(dataFromServer.near_earth_objects).sort(([dateA], [dateB]) => {
+        return new Date(dateA) - new Date(dateB);
+      });
+    
+      setInitialData(sortedInitialNeos);
+    } catch {
+      setError('Unable to load data. Try again later!')
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   useEffect(() => {
@@ -20,7 +31,22 @@ function App() {
   }, []);
 
   return (
-    initialData.length > 0 && (<NeosList initialData={initialData} />)
+    <>
+      { isLoading
+        ? <Loader />
+        : (initialData.length > 0 && (<NeosList initialData={initialData} />)) }
+
+      <Snackbar
+        open={error !== null}
+        autoHideDuration={5000}
+        onClose={() => setError(null)}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      >
+        <Alert severity="error">
+          {error}
+        </Alert>
+      </Snackbar>
+    </>
   );
 }
 
--- src/components/NeoList/NeosList.jsx
+++ src/components/NeoList/NeosList.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { NeoCard } from './NeoCard';
+import { NeoCard } from '../NeoCard';
 import './NeoList.scss';
 
 export const NeosList = ({ initialData }) => {
